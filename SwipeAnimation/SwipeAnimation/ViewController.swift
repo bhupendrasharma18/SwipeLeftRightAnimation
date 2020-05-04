a932e9d71db7065f312c88fa5619e2e8bac5b427
Bhupendra <bhupendrasharma18@gmail.com>
2020-05-04 18:53:52 +0530

    Basic rotation for a UIVIew with gesture swiping

--- SwipeAnimation/SwipeAnimation/ViewController.swift
+++ SwipeAnimation/SwipeAnimation/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myView = UIView()
+    var panGestureRecognizer: UIPanGestureRecognizer!
+    var xFromCenter: CGFloat!
+    var yFromCenter: CGFloat!
+    var originalPoint: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let width = UIScreen.main.bounds.width
+        view.addSubview(myView)
+        myView.frame = CGRect.init(x: (width - 300) / 2, y: 100, width: 300, height: 300)
+        myView.translatesAutoresizingMaskIntoConstraints = true
+        myView.backgroundColor = .red
+        
+        panGestureRecognizer = UIPanGestureRecognizer.init(target: self, action:  #selector(self.beingDragged(_:)))
+        myView.addGestureRecognizer(panGestureRecognizer)
     }
 
-
+    @objc func beingDragged(_ gestureRecognizer: UIPanGestureRecognizer) {
+        xFromCenter = gestureRecognizer.translation(in: self.myView).x
+        yFromCenter = gestureRecognizer.translation(in: self.myView).y
+        switch gestureRecognizer.state {
+        case .began:
+            originalPoint = myView.center
+        case .changed:
+            let ROTATION_STRENGTH = 320.0
+            let ROTATION_MAX = 1.0
+            let SCALE_STRENGTH = 4.0
+            let SCALE_MAX = 0.93
+            let rotationStrength = min(xFromCenter / CGFloat(ROTATION_STRENGTH), CGFloat(ROTATION_MAX))
+            let rotationAngel = .pi/8 * rotationStrength
+            let scale = max(1 - abs(rotationStrength) / CGFloat(SCALE_STRENGTH), CGFloat(SCALE_MAX))
+            myView.center = CGPoint(x: originalPoint.x + xFromCenter, y: originalPoint.y + yFromCenter)
+            let transforms = CGAffineTransform(rotationAngle: rotationAngel)
+            let scaleTransform: CGAffineTransform = transforms.scaledBy(x: scale, y: scale)
+            myView.transform = scaleTransform
+            
+        case .ended:
+            afterSwipe()
+            print("beingDragged ended")
+            
+        default:
+            break
+        }
+    }
+    
+    func afterSwipe() {
+        resetCardView()
+    }
+    
+    func resetCardView() {
+        UIView.animate(withDuration: 0.5) {
+            self.myView.center = self.originalPoint
+            self.myView.transform = CGAffineTransform.init(rotationAngle: 0)
+        }
+    }
 }
 
